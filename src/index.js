5acc9f5816c79a617a39874e186f8cfc238609c6
OhJingMunEo <jsdata93@gmail.com>
2023-02-21 21:11:12 +0900

    Scraping 변경

--- src/index.js
+++ src/index.js
@@ -8,50 +8,36 @@ const app = express();
 app.listen(3000, () => console.log('Server started on port 3000'));
 
 //특정 url HTML 받기
-const getHTML = async (pcode) => {
+const getHTML = async (url) => {
   try {
-    return await axios.get("https://prod.danawa.com/info/?pcode=" + pcode);
+    return await axios.get(url);
   } catch (err) {
     console.log(err);
   };
 };
 
 // 파라미터로 pcode(제품 코드) 입력 후 해당 url의 HTML 파싱 한 후 [ 해당 제품 당일 최저가 , 해당 제품 이미지 src ] return
-const parsing = async (pcode) => {
-  const html = await getHTML(pcode);
+const parsing = async (url) => {
+  const html = await getHTML(url);
   const $ = cheerio.load(html.data);
 
   //다나와 최저가 저장
-  const price = $('em[class=prc_c]', '.lwst_prc').html();
   
   //다나와 이미지 src
-  const img_src = 'https:' + $('#baseImage').attr('src').split('?')[0];
+  const p_json = $('[type=application/ld+json]').html();
+  const name = $('id=productBlog-productName').html();
+  const price = JSON.parse(p_json).offers.lowPrice;
+  const img_src = JSON.parse(p_json).image[0];
+  console.log(price);
+  console.log(img_src);
   
-  return [price,img_src];
+  return [name, price, img_src];
 };
-
-
-// 파라미터로 pcode(제품 코드) 입력 후 해당 url의 HTML 파싱 한 후 [ 해당 제품 당일 최저가 , 해당 제품 이미지 src ] return
-const make_pcode = async (p_url) => {
-  const pcode = p_url.split('code=')[1].split('&')[0]
-  console.log(pcode)
-  return pcode;
-};
-
-
 
 // pcode포함하여 get방식으로 요청시 
 app.get('/crawl', async (req, res) => {
-  const pcode = req.query.pcode; // Get the pcode from the request query
-  const [low_price, img_src]  = await parsing(pcode)
+  const url = req.query.url; // Get the pcode from the request query
+  const [name, low_price, img_src]  = await parsing(url)
 
-  res.json({price : low_price, img_src : img_src})
-});
-
-// pcode포함하여 get방식으로 요청시 
-app.get('/crawl2', async (req, res) => {
-  const p_url = req.query.p_url; // Get the pcode from the request query
-  const [low_price, img_src]  = await parsing(make_pcode(p_url))
-
-  res.json({price : low_price, img_src : img_src})
+  res.json({name: name ,price : low_price, img_src : img_src, })
 });

f6fdcd353b9f88168038f0c1869c19abbeba4739
OhJingMunEo <jsdata93@gmail.com>
2023-02-21 10:12:48 +0900

    imgsrc수정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const cheerio = require('cheerio');
 
 const app = express();
 
-
 // Start the server
 app.listen(3000, () => console.log('Server started on port 3000'));
 
@@ -17,28 +16,25 @@ const getHTML = async (pcode) => {
   };
 };
 
-//HTML 파싱
+// 파라미터로 pcode(제품 코드) 입력 후 해당 url의 HTML 파싱 한 후 [ 해당 제품 당일 최저가 , 해당 제품 이미지 src ] return
 const parsing = async (pcode) => {
   const html = await getHTML(pcode);
   const $ = cheerio.load(html.data);
 
   //다나와 최저가 저장
   const price = $('em[class=prc_c]', '.lwst_prc').html();
-  console.log(price);
-
+  
   //다나와 이미지 src
-  const img_src = 'https' + $('#baseImage').attr('src').split('?')[0];
-  console.log(img_src);
+  const img_src = 'https:' + $('#baseImage').attr('src').split('?')[0];
   
   return [price,img_src];
 };
 
 
-// Endpoint to handle the URL request
+// pcode포함하여 get방식으로 요청시 
 app.get('/crawl', async (req, res) => {
   const pcode = req.query.pcode; // Get the pcode from the request query
   const [low_price, img_src]  = await parsing(pcode)
-
 
   res.json({price : low_price, img_src : img_src})
 });

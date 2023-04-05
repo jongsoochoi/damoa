40d1f1095ba2ffe477460c0c541ebe40d214de7b
OhJingMunEo <52847104+jongsoochoi@users.noreply.github.com>
2023-04-05 23:58:15 +0900

    Delete dbController.js

--- src/dbController.js
+++
@@ -1,9 +0,0 @@
-require('./prodInfoSchema.js');
-
-exports.save_prod_info = (pcode, img_src) => {
-  pro_info = new Pro_Info({
-    pcode: pcode,
-    img_src: img_src
-  })
-  pro_info.save().then(() => console.log('data_save'));
-}

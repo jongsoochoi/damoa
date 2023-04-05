434dc4b4d145d633da71e2af25fa91867bc70926
OhJingMunEo <52847104+jongsoochoi@users.noreply.github.com>
2023-04-06 00:00:03 +0900

    Update test_datas.js

--- src/test_datas.js
+++ src/test_datas.js
@@ -1,4 +1,6 @@
 const moment = require('moment');
+const Pro_info = require(`./prodInfoSchema.js`);
+
 
 //테스트용 가격 입력
 function input_test_data(prices) {
@@ -6,26 +8,41 @@ function input_test_data(prices) {
         const test_date = Number(moment().subtract(Math.floor(Math.random() * 3) + 3 * i, 'days').format('YYYYMMDD'));
         const test_lowPrice = Number(prices[0].low_price) + Math.floor(Math.random() * 1000) - 500;
 
-        prices.push({ date: test_date, low_price: test_lowPrice });
+        prices.unshift({ date: test_date, low_price: test_lowPrice });
     }
     return prices
 }
 
 // 인접한 데이터의 low_price가 같다면 date를 비교해서 작은 date 값을 가지고 있는 것만 남긴다 
 function removeDuplicatePrices(prices) {
+    prices.sort((a, b) => a.date - b.date);
 
     for (let i = 0; i < prices.length - 1; i++) {
-        if (prices[i].low_price === prices[i + 1].low_price) {
-            if (prices[i].date > prices[i + 1].date) {
-                prices.splice(i, 1);
-            } else {
+        if (prices[i].date === prices[i + 1].date) {
+            console.log(`날짜 ${prices[i].date} 와 ${prices[i + 1].date}`);
+            console.log(`가격 ${prices[i].low_price} 와 ${prices[i + 1].low_price}`);
+            if (prices[i].low_price > prices[i + 1].low_price) {
                 prices.splice(i + 1, 1);
+            } else {
+                prices.splice(i, 1);
+            };
+            i--;
+        };
+        if (prices[i].low_price === prices[i + 1].low_price) {
+            console.log(`날짜 ${prices[i].date} 와 ${prices[i + 1].date}`);
+            console.log(`가격 ${prices[i].low_price} 와 ${prices[i + 1].low_price}`);
+            if (prices[i].date < prices[i + 1].date) {
+                prices.splice(i + 1, 1);
+            } else {
+                prices.splice(i, 1);
             };
             i--;
         };
     };
+
     return prices;
 }
+
 
 
 module.exports.input_test_data = input_test_data;

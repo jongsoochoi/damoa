610273b47fbc2456bc51bbabce5181071d6e4f1b
OhJingMunEo <52847104+jongsoochoi@users.noreply.github.com>
2023-05-14 17:32:21 +0900

    Update scraping.js

--- src/scraping.js
+++ src/scraping.js
@@ -4,54 +4,54 @@ const Pro_info = require(`./prodInfoSchema.js`);
 const moment = require('moment');
 
 //특정 url HTML 받기
-const getHTML = async (url) => {
-    try {
-      return await axios({
-        method: "get",
-        url: url
-      });
-    } catch (err) {
-      console.log(err);
-    };
+async function getHTML(url) {
+  try {
+    return await axios({
+      method: "get",
+      url: url
+    });
+  } catch (err) {
+    console.log(err);
   };
-  
-  // 파라미터로 pcode(제품 코드) 입력 후 해당 url의 HTML 파싱 한 후 [ 해당 제품 당일 최저가 , 해당 제품 이미지 src ] return
-  const parsing = async (p_code, p_url) => {
+};
 
-    let url = "";
+// 파라미터로 pcode(제품 코드) 입력 후 해당 url의 HTML 파싱 한 후 [ 해당 제품 당일 최저가 , 해당 제품 이미지 src ] return
+async function parsing(p_code, p_url) {
 
-    if(p_code != null) {
-        console.log("pcode 입력 : " + p_code);
-        url = "https://prod.danawa.com/info/?pcode=" + p_code;
-        console.log(url);
-    } else if (p_url != null) {
-        console.log("p_url 입력 : " + p_url);
-        url = "https://danawa.page.link/" + p_url;
-        console.log(url);
-    };
+  let url = "";
 
-    const html = await getHTML(url);
-    const $ = cheerio.load(html.data);
-
-    //리턴값
-    const pro_info = new Pro_info();
-  
-    // 제품 JSON
-    const p_json = JSON.parse($('[type=application/ld+json]').html());
-  
-    // 다나와 제품 코드 저장
-    pro_info.pcode = Number(p_json.sku);
-  
-    // 다나와 제품명 저장
-    pro_info.name = p_json.name;
-  
-    // 다나와 이미지 src 저장
-    pro_info.img_src = "https:" + p_json.image[0];
-  
-    // 다나와 최저가 저장
-    pro_info.prices.push({low_price : Number(p_json.offers.lowPrice), date : Number(moment().add(9, 'hours').format(`YYYYMMDD`))});
-
-    return pro_info;
+  if (p_code) {
+    console.log("pcode 입력 : " + p_code);
+    url = "https://prod.danawa.com/info/?pcode=" + p_code;
+  } else if (p_url) {
+    console.log("p_url 입력 : " + p_url);
+    url = "https://danawa.page.link/" + p_url;
+  } else {
+    return false;
   };
+
+  const html = await getHTML(url);
+  const $ = cheerio.load(html.data);
+
+  //리턴값
+  const pro_info = new Pro_info();
+
+  // 제품 JSON
+  const p_json = JSON.parse($('[type=application/ld+json]').html());
+
+  // 다나와 제품 코드 저장
+  pro_info.pcode = Number(p_json.sku);
+
+  // 다나와 제품명 저장
+  pro_info.name = p_json.name;
+
+  // 다나와 이미지 src 저장
+  pro_info.img_src = "https:" + p_json.image[0];
+
+  // 다나와 최저가 저장
+  pro_info.prices.push({ low_price: Number(p_json.offers.lowPrice), date: Number(moment().add(9, 'hours').format(`YYYYMMDD`)) });
+
+  return pro_info;
+};
 
 module.exports.parsing = parsing;

1fc26e4fba00946bb6b3c60c928dbc041038dcaf
OhJingMunEo <52847104+jongsoochoi@users.noreply.github.com>
2023-05-14 17:32:02 +0900

    Update prodInfoSchema.js

--- src/prodInfoSchema.js
+++ src/prodInfoSchema.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
+
 const lowPriceSchema = new mongoose.Schema({
   date: { type: Number},
   low_price: {type: Number},
   _id : false
 });
 
+
 const prodInfoSchema = new mongoose.Schema({
   pcode: { type: Number, required: true, unique: true },
   name: { type: String, required: true},
   img_src: { type: String, required: true},
   save_date: { type: Date, default: Date.now},
-  prices: [lowPriceSchema],
+  prices: [lowPriceSchema]
 });
+
 
 module.exports = mongoose.model('Prod_Info', prodInfoSchema);
